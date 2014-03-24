321310d4a6c72b129209e326620d07a58be5d812
mars <occultknight@gmail.com>
2014-03-24 15:43:23 +0800

    add simple draggable behavior

--- src/js/ui/Behavior.js
+++ src/js/ui/Behavior.js
@@ -1,12 +1,4 @@
 (function (nx) {
     nx.define('nx.ui.Behavior', nx.ui.AbstractComponent, {
-        methods: {
-            attach: function (parent) {
-
-            },
-            detach: function () {
-
-            }
-        }
     });
 })(nx);
