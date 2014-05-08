1b1e3f46b0fe5035ce32505dd0708c98ba632654
lkang2 <lkang2@cisco.com>
2014-05-08 15:01:47 +0800

    Add BezierCurve.slice and BezierCurve.breakdown

--- src/js/geometry/BezierCurve.js
+++ src/js/geometry/BezierCurve.js
@@ -1,44 +1,126 @@
-(function (nx, ui, global) {
+(function(nx, ui, global) {
     /**
      * @class BezierCurve
      * @namespace nx.geometry
      */
     var EXPORT = nx.define("nx.geometry.BezierCurve", nx.Observable, {
-        statics: {
-            separate: function separate(bezier, rate) {
-                var rest = 1 - rate;
-                var intervalSeparatePoint = function (interval) {
-                    return [interval[0][0] * rest + interval[1][0] * rate, interval[0][1] * rest + interval[1][1] * rate];
-                };
-                var intervalInter = function (i1, i2) {
-                    return [intervalSeparatePoint([i1[0], i2[0]]), intervalSeparatePoint([i1[1], i2[1]])];
-                };
-                var bezierLower = function (bezier) {
-                    var i, rslt = [];
-                    for (i = 0; i < bezier.length - 1; i++) {
-                        rslt.push(intervalInter(bezier[i], bezier[i + 1]));
-                    }
-                    return rslt;
-                };
-                // start iterate
-                var point, left = [], right = [];
-                var intervals = bezier, interval;
-                while (intervals.length) {
-                    interval = intervals[0];
-                    left.push([interval[0], intervalSeparatePoint(interval)]);
-                    interval = intervals[intervals.length - 1];
-                    right.unshift([intervalSeparatePoint(interval), interval[1]]);
-                    if (intervals.length == 1) {
-                        point = intervalSeparatePoint(intervals[0]);
-                    }
-                    intervals = bezierLower(intervals);
+        statics: (function() {
+            function transformBezierToPolyline(bezier) {
+                var i, polyline = [];
+                for (i = 0; i < bezier.length - 1; i++) {
+                    polyline.push([bezier[i], bezier[i + 1]]);
                 }
-                return {
-                    point: point,
-                    left: left,
-                    right: right
-                };
+                return polyline;
             }
-        }
+
+            function transformPolylineToBezier(polyline) {
+                var i, bezier = [polyline[0][0]];
+                for (i = 0; i < polyline.length; i++) {
+                    bezier.push(polyline[i][1]);
+                }
+                return bezier;
+            }
+
+            function transformRecursiveSeparatePoints(points) {
+                var i = 0,
+                    last = 0,
+                    result = [];
+                for (i = 0; i < points.length; i++) {
+                    if (typeof points[i] !== "number" || points[i] <= last || points[i] >= 1) {
+                        throw "Invalid bread point list: " + points.join(",");
+                    }
+                    result.push((points[i] - last) / (1 - last));
+                    last = points[i];
+                }
+                return result;
+            }
+            return {
+                slice: function(bezier, from, to) {
+                    if (from === 0) {
+                        if (to === 0) {
+                            return null;
+                        }
+                        return EXPORT.breakdown(bezier, to).beziers[0];
+                    } else if (!to) {
+                        return EXPORT.breakdown(bezier, from).beziers[1];
+                    } else {
+                        return EXPORT.breakdown(bezier, from, to).beziers[1];
+                    }
+                },
+                breakdown: function(bezier) {
+                    // get the rest arguments
+                    var rates = Array.prototype.slice.call(arguments, 1);
+                    if (!rates.length) {
+                        throw "Invalid argument length: " + arguments.length;
+                    }
+                    rates = transformRecursiveSeparatePoints(rates);
+                    var rate, polyline, sep, points = [bezier[0]],
+                        beziers = [];
+                    // transform bezier points into lines
+                    polyline = transformBezierToPolyline(bezier);
+                    // iterate all rates
+                    while (rates.length) {
+                        // get the separate ratio
+                        rate = rates.shift();
+                        // separate the rest bezier
+                        sep = EXPORT.separate(polyline, rate);
+                        // mark the points and beziers
+                        points.push(sep.point);
+                        beziers.push(transformPolylineToBezier(sep.left));
+                        // get the rest
+                        polyline = sep.right;
+                    }
+                    // append the rest bezier
+                    points.push(bezier[bezier.length - 1]);
+                    beziers.push(transformPolylineToBezier(polyline));
+                    return {
+                        points: points,
+                        beziers: beziers
+                    };
+                },
+                /**
+                 * @method separate
+                 * @param polyline List of intervals (interval=[point-from, point-to], point=[x, y]).
+                 * @param rate The rate to separate.
+                 * @return {point:[x, y], left: leftPolyline, right: rightPolyline}
+                 */
+                separate: function separate(polyline, rate) {
+                    var rest = 1 - rate;
+                    var intervalSeparatePoint = function(interval) {
+                        return [interval[0][0] * rest + interval[1][0] * rate, interval[0][1] * rest + interval[1][1] * rate];
+                    };
+                    var intervalInter = function(i1, i2) {
+                        return [intervalSeparatePoint([i1[0], i2[0]]), intervalSeparatePoint([i1[1], i2[1]])];
+                    };
+                    var polylineLower = function(polyline) {
+                        var i, rslt = [];
+                        for (i = 0; i < polyline.length - 1; i++) {
+                            rslt.push(intervalInter(polyline[i], polyline[i + 1]));
+                        }
+                        return rslt;
+                    };
+                    // start iterate
+                    var point, left = [],
+                        right = [];
+                    var intervals = polyline,
+                        interval;
+                    while (intervals.length) {
+                        interval = intervals[0];
+                        left.push([interval[0], intervalSeparatePoint(interval)]);
+                        interval = intervals[intervals.length - 1];
+                        right.unshift([intervalSeparatePoint(interval), interval[1]]);
+                        if (intervals.length == 1) {
+                            point = intervalSeparatePoint(intervals[0]);
+                        }
+                        intervals = polylineLower(intervals);
+                    }
+                    return {
+                        point: point,
+                        left: left,
+                        right: right
+                    };
+                };
+            };
+        })()
     });
 })(nx, nx.ui, window);
