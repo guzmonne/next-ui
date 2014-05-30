469c319ea020cef07f19703664345092aff6a1c8
lkang2 <lkang2@cisco.com>
2014-05-30 10:44:40 +0800

    Add keyword 'binding'. Add dependencies/update/value support.

---
+++ src/keyword.js
@@ -0,0 +1,144 @@
+(function (nx) {
+    var keyword = nx.keyword = nx.keyword || {
+        binding: function (source, callback, async) {
+            if (typeof source !== "string") {
+                callback = source.callback;
+                async = source.async;
+                source = source.source;
+            }
+            return new nx.keyword.internal.Binding({
+                source: source,
+                async: async,
+                callback: callback
+            });
+        },
+        internal: {
+            idle: function () {},
+            watch: (function () {
+                var single = function (o, path, listener, context) {
+                    var keys = path.split(".");
+
+                    function level(parent, idx) {
+                        if (parent && idx < keys.length) {
+                            var key = keys[idx];
+                            // watch on the collection changes
+                            if (key == "*" || key == "%") {
+                                // TODO handler watching on collection changes
+                            }
+                            else {
+                                var child = nx.path(parent, key);
+                                if (parent.watch) {
+                                    var pathRest = keys.slice(idx + 1).join("."),
+                                        childUnwatch = level(child, idx + 1);
+                                    var watcher = function (pname, pnewvalue, poldvalue) {
+                                        var newvalue = pathRest ? nx.path(pnewvalue, pathRest) : pnewvalue;
+                                        var oldvalue = pathRest ? nx.path(poldvalue, pathRest) : poldvalue;
+                                        listener.call(context || o, path, newvalue, oldvalue);
+                                        if (pnewvalue !== child) {
+                                            childUnwatch();
+                                            child = pnewvalue;
+                                            childUnwatch = level(child, idx + 1);
+                                        }
+                                    };
+                                    parent.watch(key, watcher, parent);
+                                    return function () {
+                                        childUnwatch();
+                                        parent.unwatch(key, watcher, parent);
+                                    };
+                                }
+                                else if (child) {
+                                    return level(child, idx + 1);
+                                }
+                            }
+                        }
+                        return keyword.internal.idle;
+                    }
+                    var unwatch = level(o, 0);
+                    return {
+                        unwatch: unwatch,
+                        notify: function () {
+                            var value = nx.path(o, path);
+                            listener.call(context || o, path, value, value);
+                        }
+                    };
+                };
+                return function (target, paths, update) {
+                    if (!target || !paths || !update) {
+                        return;
+                    }
+                    // apply the watching
+                    var deps;
+                    if (nx.is(paths, "String")) {
+                        deps = paths.replace(/\s/g, "").split(",");
+                    }
+                    else {
+                        deps = paths;
+                    }
+                    nx.each(deps, function (v, i) {
+                        if (/^\d+$/.test(v)) {
+                            deps[i] = v * 1;
+                        }
+                    });
+                    var unwatchers = [],
+                        vals = [];
+                    var notify = function (key) {
+                        var values = vals.slice();
+                        values.push(key);
+                        update.apply(target, values);
+                    };
+                    for (i = 0; i < deps.length; i++) {
+                        (function (idx) {
+                            vals[idx] = nx.path(target, deps[idx]);
+                            var unwatcher = single(target, deps[idx], function (path, value) {
+                                vals[idx] = value;
+                                notify(deps[idx]);
+                            });
+                            unwatchers.push(unwatcher);
+                        })(i);
+                    }
+                    return {
+                        notify: notify,
+                        uncascade: function () {
+                            while (unwatchers.length) {
+                                unwatchers.shift().unwatch();
+                            }
+                        }
+                    };
+                };
+            })(),
+            Binding: (function () {
+                var Binding = function (options) {
+                    this.source = options.source;
+                    this.async = options.async;
+                    this.callback = options.callback;
+                };
+                Binding.prototype = {
+                    apply: function (o, pname) {
+                        var binding = {
+                            owner: o,
+                            property: pname,
+                            set: o && pname && function (v) {
+                                o.set(pname, v);
+                            }
+                        };
+                        var watching = nx.keyword.internal.watch(o, this.source, function () {
+                            var rslt;
+                            if (this.callback) {
+                                rslt = this.callback.apply(binding, arguments);
+                            }
+                            else {
+                                rslt = arguments[0];
+                            }
+                            if (!this.async) {
+                                binding.set(rslt);
+                            }
+                        }.bind(this));
+                        watching.notify();
+                        return watching;
+                    }
+                }
+                return Binding;
+            })()
+        }
+    };
+})(nx);
---
+++ test/binding.js
@@ -0,0 +1,110 @@
+module("binding.js");
+
+nx.define('test.SimpleBindingClass', nx.Observable, {
+    properties: {
+        prop: 10,
+        prop1: {
+            value: nx.keyword.binding("prop")
+        },
+        prop2: {
+            value: nx.keyword.binding("prop1", function (prop1) {
+                return prop1 * 2;
+            })
+        },
+        prop3: {
+            value: nx.keyword.binding({
+                source: "prop1, prop2",
+                callback: function (prop1, prop2) {
+                    return prop1 + prop2;
+                }
+            })
+        }
+    }
+});
+
+test('keyword binding', function () {
+    var a = new test.SimpleBindingClass();
+    ok(a.prop1() === 10, "initial binding single");
+    ok(a.prop2() === 20, "initial binding with argument");
+    ok(a.prop3() === 30, "initial binding with configure");
+    a.prop(20);
+    ok(a.prop1() === 20, "binding single");
+    ok(a.prop2() === 40, "binding with argument");
+    ok(a.prop3() === 60, "binding with configure");
+});
+
+nx.define("test.PathBindingClass", nx.Observable, {
+    properties: {
+        simple: {
+            value: function () {
+                return new test.SimpleBindingClass();
+            }
+        },
+        prop1: {
+            value: nx.keyword.binding("simple.prop")
+        },
+        prop2: {
+            value: nx.keyword.binding("simple.prop", function (prop1) {
+                return prop1 * 2;
+            })
+        },
+        prop3: {
+            value: nx.keyword.binding({
+                source: "simple.prop1, simple.prop2",
+                callback: function (prop1, prop2) {
+                    return prop1 + prop2;
+                }
+            })
+        }
+    }
+});
+
+test('keyword binding with path', function () {
+    var a = new test.PathBindingClass();
+    ok(a.prop1() === 10, "initial binding single");
+    ok(a.prop2() === 20, "initial binding with argument");
+    ok(a.prop3() === 30, "initial binding with configure");
+    a.simple().prop(20);
+    ok(a.prop1() === 20, "binding single");
+    ok(a.prop2() === 40, "binding with argument");
+    ok(a.prop3() === 60, "binding with configure");
+});
+
+nx.define("test.DependBindingClass", nx.Observable, {
+    properties: {
+        simple: {
+            value: function () {
+                return new test.SimpleBindingClass();
+            }
+        },
+        prop1: {
+            dependencies: "simple.prop"
+        },
+        prop2: {
+            dependencies: ["simple.prop1"],
+            update: function (prop1) {
+                ok(true, "update will surely be called");
+            },
+            value: function (prop1) {
+                return prop1 * 2;
+            }
+        },
+        prop3: {
+            dependencies: ["simple.prop1", "simple.prop2"],
+            update: function (prop1, prop2) {
+                this.set(prop1 + prop2);
+            }
+        }
+    }
+});
+
+test('keyword binding dependencies with path', function () {
+    var a = new test.DependBindingClass();
+    ok(a.prop1() === 10, "initial binding single");
+    ok(a.prop2() === 20, "initial binding with argument");
+    ok(a.prop3() === 30, "initial binding with configure");
+    a.simple().prop(20);
+    ok(a.prop1() === 20, "binding single");
+    ok(a.prop2() === 40, "binding with argument");
+    ok(a.prop3() === 60, "binding with configure");
+});
