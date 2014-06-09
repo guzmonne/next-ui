b1a58256cf965bbd72d4ed5637bb7275c43b77d4
lkang2 <lkang2@cisco.com>
2014-06-09 09:31:48 +0800

    Add keyMap property to nx.Env

--- src/js/Env.js
+++ src/js/Env.js
@@ -58,15 +58,78 @@
     };
 
 
+    var keyMap = {
+        BACKSPACE: 8,
+        TAB: 9,
+        CLEAR: 12,
+        ENTER: 13,
+        SHIFT: 16,
+        CTRL: 17,
+        ALT: 18,
+        META: (nx.Env.browser().name === "chrome" || nx.Env.browser().name === "webkit" || nx.Env.browser().name === "safari") ? 91 : 224, // the apple key on macs
+        PAUSE: 19,
+        CAPS_LOCK: 20,
+        ESCAPE: 27,
+        SPACE: 32,
+        PAGE_UP: 33,
+        PAGE_DOWN: 34,
+        END: 35,
+        HOME: 36,
+        LEFT_ARROW: 37,
+        UP_ARROW: 38,
+        RIGHT_ARROW: 39,
+        DOWN_ARROW: 40,
+        INSERT: 45,
+        DELETE: 46,
+        HELP: 47,
+        LEFT_WINDOW: 91,
+        RIGHT_WINDOW: 92,
+        SELECT: 93,
+        NUMPAD_0: 96,
+        NUMPAD_1: 97,
+        NUMPAD_2: 98,
+        NUMPAD_3: 99,
+        NUMPAD_4: 100,
+        NUMPAD_5: 101,
+        NUMPAD_6: 102,
+        NUMPAD_7: 103,
+        NUMPAD_8: 104,
+        NUMPAD_9: 105,
+        NUMPAD_MULTIPLY: 106,
+        NUMPAD_PLUS: 107,
+        NUMPAD_ENTER: 108,
+        NUMPAD_MINUS: 109,
+        NUMPAD_PERIOD: 110,
+        NUMPAD_DIVIDE: 111,
+        F1: 112,
+        F2: 113,
+        F3: 114,
+        F4: 115,
+        F5: 116,
+        F6: 117,
+        F7: 118,
+        F8: 119,
+        F9: 120,
+        F10: 121,
+        F11: 122,
+        F12: 123,
+        F13: 124,
+        F14: 125,
+        F15: 126,
+        NUM_LOCK: 144,
+        SCROLL_LOCK: 145
+    };
+
+
     var supportMap = {
-        addEventListener: !!document.addEventListener,
-        dispatchEvent: !!document.dispatchEvent,
-        getBoundingClientRect: !!document.documentElement.getBoundingClientRect,
+        addEventListener: !! document.addEventListener,
+        dispatchEvent: !! document.dispatchEvent,
+        getBoundingClientRect: !! document.documentElement.getBoundingClientRect,
         onmousewheel: 'onmousewheel' in document,
-        XDomainRequest: !!window.XDomainRequest,
-        crossDomain: !!( window.XDomainRequest || window.XMLHttpRequest),
+        XDomainRequest: !! window.XDomainRequest,
+        crossDomain: !! (window.XDomainRequest || window.XMLHttpRequest),
         getComputedStyle: 'getComputedStyle' in window,
-        iePropertyChange: !!(ie && ie < 9),
+        iePropertyChange: !! (ie && ie < 9),
         w3cChange: !ie || ie > 8,
         w3cFocus: !ie || ie > 8,
         w3cInput: !ie || ie > 9,
@@ -75,7 +138,7 @@
         cssFloat: 'cssFloat' in tempStyle,
         opacity: (/^0.55$/).test(tempStyle.opacity),
         filter: 'filter' in tempStyle,
-        classList: !!tempElement.classList,
+        classList: !! tempElement.classList,
         removeProperty: 'removeProperty' in tempStyle
     };
 
@@ -247,6 +310,15 @@
              */
             browser: {
                 value: browser
+            },
+            /**
+             * Get keyboard key code map.
+             * @property keyMap
+             * @type {Object}
+             * @default {}
+             */
+            keyMap: {
+                value: keyMap
             }
         },
         methods: {
