7833f3e19de357ceb03d144a95c2b2e82961c353
aaikepae <aaikepae@cisco.com>
2014-06-25 22:47:31 +0800

    Add xmlns naming space

--- src/js/dom/Document.js
+++ src/js/dom/Document.js
@@ -121,7 +121,8 @@
 
     var nsMap = {
         svg: "http://www.w3.org/2000/svg",
-        xlink: "http://www.w3.org/1999/xlink"
+        xlink: "http://www.w3.org/1999/xlink",
+        xmlns: "http://www.w3.org/1999/xhtml"
     };
 
     /**
