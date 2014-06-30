239cc0767c56db00b755b919b441b76140be7584
aaikepae <aaikepae@cisco.com>
2014-06-30 14:17:23 +0800

    Fix node can't be drag when not support multiple links

--- src/js/topology/link/LinkSet.js
+++ src/js/topology/link/LinkSet.js
@@ -163,7 +163,7 @@
              */
             updateLinkSet: function () {
                 var value = this._processPropertyValue(this.collapsedRule());
-                this.activated(value);
+                this.model().activated(value, {force: true});
                 if (value) {
                     this.append();
                     this.update();
