b0a7c6dcf1110a6856a287814f174b2b8cb86eee
weqiao <Lost2pbrepre>
2014-07-07 10:49:07 +0800

    add test case for topo

--- test/wei/js/mainFrameViewModel.js
+++ test/wei/js/mainFrameViewModel.js
@@ -27,8 +27,8 @@ nx.define('topo.test.ViewModel', nx.Observable, {
         failed: {
             value: []
         },
-        menustatus:{
-            value:'btn-group'
+        menustatus: {
+            value: 'btn-group'
         }
 
 
@@ -45,7 +45,7 @@ nx.define('topo.test.ViewModel', nx.Observable, {
             this.index(0);
         },
         casePass: function (sender, event) {
-            test(this.testname(),function(){
+            test(this.testname(), function () {
                 ok(true);
             })
             this.topoRestore()
@@ -54,12 +54,14 @@ nx.define('topo.test.ViewModel', nx.Observable, {
 //            app.fire('generateReport');
         },
         caseFail: function (sender, event) {
-            test(this.testname(),function(){
+            test(this.testname(), function () {
                 ok(false);
             })
-            this.topoRestore()
+            this.topoRestore();
             this.failed().push(this.testname());
             this.nextcase(this.index() + 1);
+
+
         },
         nextcase: function (index) {
             if (index < this.tcase().length) {
@@ -74,56 +76,57 @@ nx.define('topo.test.ViewModel', nx.Observable, {
                 console.log('failed :  ' + this.failed())
             }
         },
-        jumpTo: function(data)
-        {
+        jumpTo: function (data) {
+            this.script(null);
             this.topoRestore();
             this.description(data.description);
             this.script(data.script);
             this.testname(data.name);
         },
-        menuaction:  function()
-        {
-            if (this.menustatus()=='btn-group')
+        menuaction: function () {
+            if (this.menustatus() == 'btn-group')
                 this.menustatus('btn-group open');
             else
                 this.menustatus('btn-group');
         },
         topoRestore: function () {
-            console.log('restore');
+//            console.log('restore');
 //            this.topo().adaptToContainer();
-            this.topo().setData({
-                nodes: [
-                    {"id": 0, "x": 410, "y": 100, "name": "12K-1"},
-                    {"id": 1, "x": 410, "y": 280, "name": "12K-2"},
-                    {"id": 2, "x": 660, "y": 280, "name": "Of-9k-03"},
-                    {"id": 3, "x": 660, "y": 100, "name": "Of-9k-02"},
-                    {"id": 4, "x": 180, "y": 190, "name": "Of-9k-01"}
-                ],
-                links: [
-                    {"id": 0, "source": 0, "target": 1},
-                    {"id": 1, "source": 1, "target": 2},
-                    {"id": 2, "source": 1, "target": 3},
-                    {"id": 3, "source": 4, "target": 1},
-                    {"id": 4, "source": 2, "target": 3},
-                    {"id": 5, "source": 2, "target": 0},
-                    {"id": 6, "source": 3, "target": 0},
-                    {"id": 7, "source": 3, "target": 0},
-                    {"id": 8, "source": 3, "target": 0},
-                    {"id": 9, "source": 0, "target": 4},
-                    {"id": 10, "source": 0, "target": 4},
-                    {"id": 11, "source": 0, "target": 3}
-                ]
-            });
-            this.topo().adaptToContainer();
+//            this.topo().setData({
+//                nodes: [
+//                    {"id": 0, "x": 410, "y": 100, "name": "12K-1"},
+//                    {"id": 1, "x": 410, "y": 280, "name": "12K-2"},
+//                    {"id": 2, "x": 660, "y": 280, "name": "Of-9k-03"},
+//                    {"id": 3, "x": 660, "y": 100, "name": "Of-9k-02"},
+//                    {"id": 4, "x": 180, "y": 190, "name": "Of-9k-01"}
+//                ],
+//                links: [
+//                    {"id": 0, "source": 0, "target": 1},
+//                    {"id": 1, "source": 1, "target": 2},
+//                    {"id": 2, "source": 1, "target": 3},
+//                    {"id": 3, "source": 4, "target": 1},
+//                    {"id": 4, "source": 2, "target": 3},
+//                    {"id": 5, "source": 2, "target": 0},
+//                    {"id": 6, "source": 3, "target": 0},
+//                    {"id": 7, "source": 3, "target": 0},
+//                    {"id": 8, "source": 3, "target": 0},
+//                    {"id": 9, "source": 0, "target": 4},
+//                    {"id": 10, "source": 0, "target": 4},
+//                    {"id": 11, "source": 0, "target": 3}
+//                ]
+//            });
+//            this.topo().adaptToContainer();
         },
         executeScript: function () {
             //this.topo().on('topologyGenerated', function () {
             try {
-                var result = this.script()(this.topo());
-                if (result)
-                    this.output(result);
-                else
-                    this.output('');
+                if (this.script()) {
+                    var result = this.script()(this.topo());
+                    if (result)
+                        this.output(result);
+                    else
+                        this.output('');
+                }
             }
             catch (err) {
                 this.output(err.message)
--- test/wei/test/testCase.js
+++ test/wei/test/testCase.js
@@ -76,7 +76,8 @@ var topoCase = [
         name: 'API resize 500,500/no fit',
         description: "resize the topo",
         script: function (topo) {
-            topo.resize(500,500);
+            topo.adaptToContainer();
+            topo.resize(500, 500);
 //            topo.fit();
             return topo.width() + ',' + topo.height();
 
@@ -87,16 +88,18 @@ var topoCase = [
         name: 'API resize 1,1, fit',
         description: "resize the topo",
         script: function (topo) {
-            topo.resize(1,1);
+            topo.adaptToContainer();
+            topo.resize(1, 1);
             topo.fit();
             return topo.width() + ',' + topo.height();
         }
     },
     {
-        name: 'API resize 0,0/no fit',
+        name: 'API resize 0,0/fit',
         description: "resize the topo",
         script: function (topo) {
-            topo.resize(0,0);
+            topo.adaptToContainer();
+            topo.resize(0, 0);
             topo.fit();
             return topo.width() + ',' + topo.height();
             //topo.adaptToContainer();
@@ -106,8 +109,9 @@ var topoCase = [
         name: 'API resize 3000,3000/no fit',
         description: "resize the topo",
         script: function (topo) {
-            topo.resize(3000,3000);
-            //topo.fit();
+            topo.adaptToContainer();
+            topo.resize(3000, 3000);
+            topo.fit();
             return topo.width() + ',' + topo.height();
             //topo.adaptToContainer();
         }
@@ -116,35 +120,175 @@ var topoCase = [
         name: 'API resize twice',
         description: "resize the topo twice",
         script: function (topo) {
-            topo.resize(500,500);
-            topo.setData({
-                nodes: [
-                    {"id": 0, "x": 410, "y": 100, "name": "12K-1"},
-                    {"id": 1, "x": 410, "y": 280, "name": "12K-2"},
-                    {"id": 2, "x": 660, "y": 280, "name": "Of-9k-03"},
-                    {"id": 3, "x": 660, "y": 100, "name": "Of-9k-02"},
-                    {"id": 4, "x": 180, "y": 190, "name": "Of-9k-01"}
-                ],
-                links: [
-                    {"id": 0, "source": 0, "target": 1},
-                    {"id": 1, "source": 1, "target": 2},
-                    {"id": 2, "source": 1, "target": 3},
-                    {"id": 3, "source": 4, "target": 1},
-                    {"id": 4, "source": 2, "target": 3},
-                    {"id": 5, "source": 2, "target": 0},
-                    {"id": 6, "source": 3, "target": 0},
-                    {"id": 7, "source": 3, "target": 0},
-                    {"id": 8, "source": 3, "target": 0},
-                    {"id": 9, "source": 0, "target": 4},
-                    {"id": 10, "source": 0, "target": 4},
-                    {"id": 11, "source": 0, "target": 3}
-                ]
-            });
             topo.adaptToContainer();
-            topo.resize(600,600);
+            topo.resize(500, 500);
+            topo.fit();
+            topo.resize(600, 600);
             topo.fit();
             return topo.width() + ',' + topo.height();
             //topo.adaptToContainer();
         }
+    },
+    {
+        name: 'API eachlink',
+        description: "eachlink",
+        script: function (topo) {
+            var output = "";
+            topo.eachLink(function (link) {
+                output = output + '//id: ' + link.id() + " source: " + link.sourceNode().label() + " dest: " + link.targetNode().label() + ' ';
+
+            })
+            return output;
+        }
+    },
+    {
+        name: 'API insertData',
+        description: "insert a normal data",
+        script: function (topo) {
+            topo.insertData({
+                nodes: [
+                    {"id": 5, "x": 500, "y": 500, "name": "qiaowei"}
+                ],
+                links: [
+                    {"id": 12, "source": 0, "target": 5}
+                ]
+            })
+        }
+    },
+    {
+        name: 'API insertData({})',
+        description: "insert a {} data",
+        script: function (topo) {
+            topo.insertData({});
+        }
+    },
+    {
+        name: 'API insertData(nodes: [],links: [])',
+        description: "insert a nodes: [],links: [] data",
+        script: function (topo) {
+            topo.insertData({nodes: [], links: []});
+        }
+    },
+    {
+        name: 'API add link, normal data',
+        description: "add a link, from 9k-01 to 9k-03",
+        script: function (topo) {
+            topo.addLink({"id": 12, "source": 4, "target": 2})
+        }
+    },
+    {
+        name: 'API add link, normal data, mulitple times',
+        description: "add a link, from 9k-01 to 9k-03",
+        script: function (topo) {
+            topo.addLink({"id": 12, "source": 4, "target": 2})
+            topo.addLink({"id": 13, "source": 4, "target": 2})
+            topo.addLink({"id": 14, "source": 4, "target": 2})
+            topo.addLink({"id": 15, "source": 4, "target": 2})
+            topo.addLink({"id": 16, "source": 4, "target": 2})
+            topo.addLink({"id": 17, "source": 4, "target": 2})
+            topo.addLink({"id": 18, "source": 4, "target": 2})
+            return topo.getData().links.length + "expect: 19"
+        }
+    },
+    {
+        name: 'API add link, null data',
+        description: "add a link, data is {}",
+        script: function (topo) {
+            topo.addLink({})
+            return topo.getData().links.length
+        }
+    },
+    {
+        name: 'API add link, dest node not exists',
+        description: "add a link, dest node not exists",
+        script: function (topo) {
+            topo.addLink({"id": 12, "source": 4, "target": 5})
+            return topo.getData().links.length
+        }
+    },
+    {
+        name: 'API add link, src node not exists',
+        description: "add a link,src node not exist",
+        script: function (topo) {
+            topo.addLink({"id": 12, "source": 5, "target": 4})
+            return topo.getData().links.length
+        }
+    },
+    {
+        name: 'API eachNode',
+        description: "eachNode",
+        script: function (topo) {
+            var output = "";
+            topo.eachNode(function (node) {
+                output = output + '//id: ' + node.id() + " label : " + node.label() + ' ';
+
+            });
+            return output;
+        }
+    },
+    {
+        name: 'API move-x',
+        description: "move",
+        script: function (topo) {
+            topo.move(-100, 0);
+        }
+    },
+    {
+        name: 'API move-y',
+        description: "move",
+        script: function (topo) {
+            topo.move(0, -100);
+        }
+    },
+    {
+        name: 'API move-2 moves',
+        description: "move x first, then move y",
+        script: function (topo) {
+            topo.move(-100, 0);
+            setTimeout(function () {
+                topo.move(0, -100);
+            }, 1000)
+
+        }
+    },
+    {
+        name: 'API move with duration 50000',
+        description: "move",
+        script: function (topo) {
+            topo.move(-100, 0, 5);
+        }
+    },
+    {
+        name: 'API move-big move',
+        description: "move out of screen",
+        script: function (topo) {
+            console.log(1111)
+            topo.move(-3000, -2000);
+        }
+    },
+    {
+        name: 'API move-monve continously',
+        description: "",
+        script: function (topo) {
+            setTimeout(function () {
+                topo.move(-100, 0);
+            }, 600)
+            setTimeout(function () {
+                topo.move(-100, 0);
+            }, 600)
+            setTimeout(function () {
+                topo.move(-100, 0);
+            }, 600)
+
+        }
+    },
+    {
+        name: 'API zoom-placeholder',
+        description: "move out of screen",
+        script: function (topo) {
+            console.log(1111)
+            topo.move(-3000, -2000);
+        }
     }
+
 ]
