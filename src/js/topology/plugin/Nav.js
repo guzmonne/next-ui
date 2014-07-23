819fea6f89489c7585aa00f872860484d97f8364
aaikepae <aaikepae@cisco.com>
2014-07-22 18:53:05 -0700

    fix CSCup16313    User can not type any search keywords when topology is full screen mode

--- src/js/topology/plugin/Nav.js
+++ src/js/topology/plugin/Nav.js
@@ -448,8 +448,8 @@
                 topo.stage().zoom(0.8, topo.adjustLayout, topo);
                 event.preventDefault();
             },
-            _full: function () {
-                this.toggleFull();
+            _full: function (sender,event) {
+                this.toggleFull(event.target);
             },
             _enterSetting: function (event) {
                 this.view("setting").addClass("n-topology-nav-setting-open");
@@ -469,20 +469,10 @@
                 }
             },
             requestFullScreen: function (el) {
-                // Supports most browsers and their versions.
-                var requestMethod = el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullScreen;
-
-                if (requestMethod) { // Native full screen.
-                    requestMethod.call(el);
-                } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
-                    var wscript = new ActiveXObject("WScript.Shell");
-                    if (wscript !== null) {
-                        wscript.SendKeys("{F11}");
-                    }
-                }
+                document.body.webkitRequestFullscreen.call(document.body);
                 return false;
             },
-            toggleFull: function () {
+            toggleFull: function (el) {
                 var elem = document.body; // Make the body go full screen.
                 var isInFullScreen = (document.fullScreenElement && document.fullScreenElement !== null) || (document.mozFullScreen || document.webkitIsFullScreen);
 
