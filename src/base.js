78ce3d13b369d45f2f90c122761982f88b8d9138
lkang2 <lkang2@cisco.com>
2014-11-07 12:30:01 +0800

    Add deep clone to nx.clone

--- src/base.js
+++ src/base.js
@@ -28,8 +28,8 @@ if (!Function.prototype.bind) {
 
 
     var isArray = Array.isArray || function (target) {
-        return target && target.constructor === Array;
-    };
+            return target && target.constructor === Array;
+        };
 
     /**
      * Extend target with properties from sources.
@@ -62,15 +62,13 @@ if (!Function.prototype.bind) {
         if (target && callback) {
             if (target.__each__) {
                 target.__each__(callback, context);
-            }
-            else {
+            } else {
                 var length = target.length;
                 if (length >= 0) {
                     for (var i = 0; i < length; i++) {
                         callback.call(context, target[i], i);
                     }
-                }
-                else {
+                } else {
                     for (var key in target) {
                         if (target.hasOwnProperty(key)) {
                             callback.call(context, target[key], key);
@@ -87,31 +85,142 @@ if (!Function.prototype.bind) {
      * @param target {Object|Array} The target object to be cloned.
      * @returns {Object} The cloned object.
      */
-    nx.clone = function (target) {
-        if (target) {
-            if (target.__clone__) {
-                return target.__clone__();
-            }
-            else {
-                if (nx.is(target, 'Array')) {
-                    return target.slice(0);
-                }
-                else {
-                    var result = {};
-                    for (var key in target) {
-                        if (target.hasOwnProperty(key)) {
-                            result[key] = target[key];
-                        }
-                    }
 
-                    return result;
+    nx.clone = (function () {
+        var deepclone = (function () {
+            var get, put, top, keys, clone;
+            get = function (map, key) {
+                for (var i = 0; i < map.length; i++) {
+                    if (map[i].key === key) {
+                        return map[i].value;
+                    }
                 }
+                return null;
+            };
+            put = function (map, key, value) {
+                var i;
+                for (i = 0; i < map.length; i++) {
+                    if (map[i].key === key) {
+                        map[i].value = value;
+                        return;
+                    }
+                }
+                map[i] = {
+                    key: key,
+                    value: value
+                };
+            };
+            top = function (stack) {
+                if (stack.length == 0) {
+                    return null;
+                }
+                return stack[stack.length - 1];
+            };
+            keys = function (obj) {
+                var keys = [];
+                if (Object.prototype.toString.call(obj) == '[object Array]') {
+                    for (var i = 0; i < obj.length; i++) {
+                        keys.push(i);
+                    }
+                } else {
+                    for (var key in obj) {
+                        keys.push(key);
+                    }
+                }
+                return keys;
+            };
+            clone = function (self) {
+                // TODO clone DOM object
+                if (window === self || document === self) {
+                    // window and document cannot be clone
+                    return null;
+                }
+                if (util.array.contain(["null", "undefined", "number", "string", "boolean", "function"], typeof self)) {
+                    return self;
+                }
+                if (!util.is.arr(self) && !util.is.obj(self)) {
+                    return self;
+                }
+                var map = [],
+                    stack = [],
+                    origin = self,
+                    dest = (util.is.arr(self) ? [] : {});
+                var stacktop, key, cached;
+                // initialize the map and stack
+                put(map, origin, dest);
+                stack.push({
+                    origin: origin,
+                    dest: dest,
+                    keys: keys(origin),
+                    idx: 0
+                });
+                while (true) {
+                    stacktop = top(stack);
+                    if (!stacktop) {
+                        // the whole object is cloned
+                        break;
+                    }
+                    origin = stacktop.origin;
+                    dest = stacktop.dest;
+                    if (stacktop.keys.length <= stacktop.idx) {
+                        // object on the stack top is cloned
+                        stack.pop();
+                        continue;
+                    }
+                    key = stacktop.keys[stacktop.idx++];
+                    // clone an object
+                    if (util.is.arr(origin[key])) {
+                        dest[key] = [];
+                    } else if (util.is.obj(origin[key])) {
+                        dest[key] = {};
+                    } else {
+                        dest[key] = origin[key];
+                        continue;
+                    }
+                    // check if needn't deep into or cloned already
+                    if (cached = get(map, origin[key])) {
+                        dest[key] = cached;
+                        continue;
+                    }
+                    // deep into the object
+                    put(map, origin[key], dest[key]);
+                    stack.push({
+                        origin: origin[key],
+                        dest: dest[key],
+                        keys: keys(origin[key]),
+                        idx: 0
+                    });
+                }
+                return dest;
+            };
+            return clone;
+        })();
+        return function (target, cfg) {
+            if (target) {
+                if (target.__clone__) {
+                    return target.__clone__(cfg);
+                } else if (!cfg) {
+                    if (nx.is(target, 'Array')) {
+                        return target.slice(0);
+                    } else {
+                        var result = {};
+                        for (var key in target) {
+                            if (target.hasOwnProperty(key)) {
+                                result[key] = target[key];
+                            }
+                        }
+
+                        return result;
+                    }
+                } else {
+                    // TODO more config options
+                    return deepclone(target);
+                }
+            } else {
+                return target;
             }
-        }
-        else {
-            return target;
-        }
-    };
+        };
+    })();
 
     /**
      * Check whether target is specified type.
@@ -123,24 +232,23 @@ if (!Function.prototype.bind) {
     nx.is = function (target, type) {
         if (target && target.__is__) {
             return target.__is__(type);
-        }
-        else {
+        } else {
             switch (type) {
-                case 'Undefined':
-                    return target === undefined;
-                case 'Null':
-                    return target === null;
-                case 'Object':
-                    return target && (typeof target === 'object');
-                case 'String':
-                case 'Boolean':
-                case 'Number':
-                case 'Function':
-                    return typeof target === type.toLowerCase();
-                case 'Array':
-                    return isArray(target);
-                default:
-                    return target instanceof type;
+            case 'Undefined':
+                return target === undefined;
+            case 'Null':
+                return target === null;
+            case 'Object':
+                return target && (typeof target === 'object');
+            case 'String':
+            case 'Boolean':
+            case 'Number':
+            case 'Function':
+                return typeof target === type.toLowerCase();
+            case 'Array':
+                return isArray(target);
+            default:
+                return target instanceof type;
             }
         }
     };
@@ -156,8 +264,7 @@ if (!Function.prototype.bind) {
         if (target) {
             if (target.__get__) {
                 return target.__get__(name);
-            }
-            else {
+            } else {
                 return target[name];
             }
         }
@@ -174,8 +281,7 @@ if (!Function.prototype.bind) {
         if (target) {
             if (target.__set__) {
                 target.__set__(name);
-            }
-            else {
+            } else {
                 target[name] = value;
             }
         }
@@ -191,8 +297,7 @@ if (!Function.prototype.bind) {
         if (target) {
             if (target.__gets__) {
                 return target.__gets__();
-            }
-            else {
+            } else {
                 var result = {};
                 for (var key in target) {
                     if (target.hasOwnProperty(key)) {
@@ -214,8 +319,7 @@ if (!Function.prototype.bind) {
         if (target && dict) {
             if (target.__sets__) {
                 target.__sets__(dict);
-            }
-            else {
+            } else {
                 for (var key in dict) {
                     if (dict.hasOwnProperty(key)) {
                         target[key] = dict[key];
@@ -236,12 +340,10 @@ if (!Function.prototype.bind) {
         if (target) {
             if (target.__has__) {
                 return target.__has__(name);
-            }
-            else {
+            } else {
                 return name in target;
             }
-        }
-        else {
+        } else {
             return false;
         }
     };
@@ -256,15 +358,12 @@ if (!Function.prototype.bind) {
     nx.compare = function (target, source) {
         if (target && target.__compare__) {
             return target.__compare__(source);
-        }
-        else {
+        } else {
             if (target === source) {
                 return 0;
-            }
-            else if (target > source) {
+            } else if (target > source) {
                 return 1;
-            }
-            else if (target < source) {
+            } else if (target < source) {
                 return -1;
             }
 
@@ -283,28 +382,27 @@ if (!Function.prototype.bind) {
     nx.path = function (target, path, value) {
         var result = target;
         if (path) {
-            var tokens = path.split('.'), token,
-                i = 0, length = tokens.length;
+            var tokens = path.split('.'),
+                token,
+                i = 0,
+                length = tokens.length;
 
             if (value === undefined) {
                 for (; result && i < length; i++) {
                     token = tokens[i];
                     if (result.__get__) {
                         result = result.__get__(token);
-                    }
-                    else {
+                    } else {
                         result = result[token];
                     }
                 }
-            }
-            else {
+            } else {
                 length -= 1;
                 for (; result && i < length; i++) {
                     token = tokens[i];
                     if (result.__get__) {
                         result = result.__get__(token);
-                    }
-                    else {
+                    } else {
                         result = result[token] = result[token] || {};
                     }
                 }
@@ -313,8 +411,7 @@ if (!Function.prototype.bind) {
                 if (result) {
                     if (result.__set__) {
                         result.__set__(token, value);
-                    }
-                    else {
+                    } else {
                         result[token] = value;
                     }
 
