0c32550850f7eff60c22aaa738e6d770aa68ea4e
lkang2 <lkang2@cisco.com>
2014-12-24 11:06:44 +0800

    Add idle/uuid core functions.

--- src/base.js
+++ src/base.js
@@ -444,4 +444,15 @@ if (!Function.prototype.bind) {
         return result;
     };
 
+    nx.idle = function () {};
+
+    nx.uuid = function () {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0,
+                v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        }).toUpperCase();
+    };
+
+
 })(nx);
