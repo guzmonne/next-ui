be3671e1bfeea13567a8ff239d00dae12c456aa4
aaikepae <aaikepae@cisco.com>
2015-01-09 15:18:24 -0800

    Fix laoding time issue

--- src/js/ui/PopupContainer.js
+++ src/js/ui/PopupContainer.js
@@ -1,41 +1,7 @@
 (function (nx, global) {
 
 
-    var Container = nx.define(nx.ui.Component, {
-        view: {
-            props: {
-                'class': 'nx n-popupContainer',
-                style: {
-                    'position': 'absolute',
-                    'top': '0px',
-                    'left': '0px'
 
-                }
-            }
-        }
-    });
-
-    /**
-     * Popup container
-     * @class nx.ui.PopupContainer
-     * @static
-     */
-
-    nx.define("nx.ui.PopupContainer", {
-        static: true,
-        properties: {
-            container: {
-                value: function () {
-                    return new Container();
-                }
-            }
-        },
-        methods: {
-            addPopup: function (popup) {
-                this.container().view().dom().appendChild(popup.view().dom());
-            }
-        }
-    });
 
 
     nx.dom.Document.ready(function () {
@@ -48,7 +14,45 @@
 
 
     setTimeout(function () {
-        if (document.body) {
+        if (document.body  && nx && nx.ui) {
+
+            var Container = nx.define(nx.ui.Component, {
+                view: {
+                    props: {
+                        'class': 'nx n-popupContainer',
+                        style: {
+                            'position': 'absolute',
+                            'top': '0px',
+                            'left': '0px'
+
+                        }
+                    }
+                }
+            });
+
+            /**
+             * Popup container
+             * @class nx.ui.PopupContainer
+             * @static
+             */
+
+            nx.define("nx.ui.PopupContainer", {
+                static: true,
+                properties: {
+                    container: {
+                        value: function () {
+                            return new Container();
+                        }
+                    }
+                },
+                methods: {
+                    addPopup: function (popup) {
+                        this.container().view().dom().appendChild(popup.view().dom());
+                    }
+                }
+            });
+
+
             if (document.body.firstChild) {
                 document.body.insertBefore(nx.ui.PopupContainer.container().view().dom().$dom, document.body.firstChild);
             } else {
