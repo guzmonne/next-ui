2bf4cc3e282100de5ea98912e5a9831d4ac55e2b
lkang2 <lkang2@cisco.com>
2015-01-13 12:47:08 +0800

    Add a table to the node tooltip example.

--- example/topology/tooltip/node.js
+++ example/topology/tooltip/node.js
@@ -1,27 +1,85 @@
 (function (nx, global) {
 
     var topologyData = {
-        nodes: [
-            {"id": 0, "x": 410, "y": 100, "name": "12K-1"},
-            {"id": 1, "x": 410, "y": 280, "name": "12K-2"},
-            {"id": 2, "x": 660, "y": 280, "name": "Of-9k-03"},
-            {"id": 3, "x": 660, "y": 100, "name": "Of-9k-02"},
-            {"id": 4, "x": 180, "y": 190, "name": "Of-9k-01"}
-        ],
-        links: [
-            {"source": 0, "target": 1},
-            {"source": 1, "target": 2},
-            {"source": 1, "target": 3},
-            {"source": 4, "target": 1},
-            {"source": 2, "target": 3},
-            {"source": 2, "target": 0},
-            {"source": 3, "target": 0},
-            {"source": 3, "target": 0},
-            {"source": 3, "target": 0},
-            {"source": 0, "target": 4},
-            {"source": 0, "target": 4},
-            {"source": 0, "target": 3}
-        ]
+        nodes: [{
+            "id": 0,
+            "x": 410,
+            "y": 100,
+            "name": "12K-1"
+        }, {
+            "id": 1,
+            "x": 410,
+            "y": 280,
+            "name": "12K-2",
+            "data": [{
+                "nodeName": "node3",
+                "packets": "12",
+                "bytes": "154",
+                "bytes": "4"
+            }, {
+                "nodeName": "node5",
+                "packets": "12",
+                "bytes": "154",
+                "bytes": "4"
+            }, {
+                "nodeName": "node1",
+                "packets": "13",
+                "bytes": "121",
+                "bytes": "5"
+            }]
+        }, {
+            "id": 2,
+            "x": 660,
+            "y": 280,
+            "name": "Of-9k-03"
+        }, {
+            "id": 3,
+            "x": 660,
+            "y": 100,
+            "name": "Of-9k-02"
+        }, {
+            "id": 4,
+            "x": 180,
+            "y": 190,
+            "name": "Of-9k-01"
+        }],
+        links: [{
+            "source": 0,
+            "target": 1
+        }, {
+            "source": 1,
+            "target": 2
+        }, {
+            "source": 1,
+            "target": 3
+        }, {
+            "source": 4,
+            "target": 1
+        }, {
+            "source": 2,
+            "target": 3
+        }, {
+            "source": 2,
+            "target": 0
+        }, {
+            "source": 3,
+            "target": 0
+        }, {
+            "source": 3,
+            "target": 0
+        }, {
+            "source": 3,
+            "target": 0
+        }, {
+            "source": 0,
+            "target": 4
+        }, {
+            "source": 0,
+            "target": 4
+        }, {
+            "source": 0,
+            "target": 3
+        }]
     };
 
 
@@ -36,29 +94,61 @@
             topology: {}
         },
         view: {
-            content: [
-                {
-                    tag: 'h1',
-                    content: '{#node.id}'
+            content: [{
+                tag: 'h1',
+                content: '{#node.id}'
+            }, {
+                tag: 'p',
+                content: [{
+                    tag: 'label',
+                    content: 'Username'
+                }, {
+                    tag: 'span',
+                    content: '{username}'
+                }]
+            }, {
+                tag: 'p',
+                content: '{#topology.width}'
+            }, {
+                tag: "table",
+                props: {
+                    class: "col-md-12",
+                    border: "1"
                 },
-                {
-                    tag: 'p',
-                    content: [
-                        {
-                            tag: 'label',
-                            content: 'Username'
-                        },
-                        {
-                            tag:'span',
-                            content:'{username}'
+                content: [{
+                    tag: "thead",
+                    content: {
+                        tag: "tr",
+                        content: [{
+                            tag: "td"
+                        }, {
+                            tag: "td",
+                            content: "pkts"
+                        }, {
+                            tag: "td",
+                            content: "bytes"
+                        }]
+                    }
+                }, {
+                    tag: "tbody",
+                    props: {
+                        items: "{#node.model.data}",
+                        template: {
+                            tag: "tr",
+                            content: [{
+                                tag: "td",
+                                content: "{nodeName}"
+                            }, {
+                                tag: "td",
+                                content: "{packets}"
+                            }, {
+                                tag: "td",
+                                content: "{bytes}"
+                            }]
                         }
-                    ]
-                },
-                {
-                    tag: 'p',
-                    content: '{#topology.width}'
-                }
-            ]
+                    }
+                }]
+            }]
         }
     });
 
@@ -92,5 +182,3 @@
     });
 
 })(nx, nx.global);
-
-
